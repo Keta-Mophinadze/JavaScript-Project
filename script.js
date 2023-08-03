8c74cfa80296bf775eda0ad3092d8e1f185bf62c
Keta Mophinadze <qeta.mophinadze@gmail.com>
2023-08-03 11:53:32 +0400

    Changes

--- script.js
+++ script.js
@@ -107,3 +107,128 @@ userNameField.addEventListener("keyup", function () {
 });
 
 
+
+
+
+
+
+
+// boooks javaaaaaascriiiiiipt
+// JavaScript
+const cardContainer = document.getElementById("card-container");
+const loadMoreButton = document.getElementById("load-more");
+const cardCountElem = document.getElementById("card-count");
+const cardTotalElem = document.getElementById("card-total");
+
+const cardLimit = 48;
+const cardIncrease = 9;
+const pageCount = Math.ceil(cardLimit / cardIncrease);
+let currentPage = 1;
+
+cardTotalElem.innerHTML = cardLimit;
+
+const cardImages = [
+  "images/1.jpeg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpg",
+  "images/5.jpg",
+  "images/6.jpg",
+  "images/7.jpg",
+  "images/8.jpeg",
+  "images/9.jpg",
+  "images/10.jpg",
+  "images/11.jpg",
+  "images/12.jpg",
+  "images/13.jpg",
+  "images/14.jpg",
+  "images/15.jpg",
+  "images/16.jpg",
+  "images/17.jpg",
+  "images/18.jpeg",
+  "images/19.jpeg",
+  "images/20.jpeg",
+  "images/21.jpeg",
+  "images/22.jpg",
+  "images/23.jpg",
+  "images/24.jpg",
+  "images/25.jpg",
+  "images/26.jpg",
+  "images/27.jpg",
+  "images/28.jpg",
+  "images/29.jpg",
+  "images/30.webp",
+  "images/31.jpg",
+  "images/32.jpg",
+  "images/33.jpg",
+  "images/34.jpg",
+  "images/35.jpg",
+  "images/36.jpg",
+  "images/37.jpg",
+  "images/38.jpeg",
+  "images/39.jpg",
+  "images/40.jpeg",
+  "images/41.jpg",
+  "images/42.jpeg",
+  "images/43.jpeg",
+  "images/44.jpg",
+  "images/45.jpg",
+  "images/46.webp",
+  "images/47.jpeg",
+  "images/48.jpeg",
+];
+
+const getRandomColor = () => {
+  const h = Math.floor(Math.random() * 360);
+  return `hsl(${h}deg, 90%, 85%)`;
+};
+
+const handleButtonStatus = () => {
+  if (pageCount === currentPage) {
+    loadMoreButton.classList.add("disabled");
+    loadMoreButton.setAttribute("disabled", true);
+  }
+};
+
+const createCard = (index, imageUrl) => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.style.backgroundImage = `url('${imageUrl}')`; // Set the background image URL for the card
+  cardContainer.appendChild(card);
+};
+
+const addCards = (pageIndex) => {
+  currentPage = pageIndex;
+
+  handleButtonStatus();
+
+  const startRange = (pageIndex - 1) * cardIncrease;
+  const endRange =
+    pageIndex * cardIncrease > cardLimit ? cardLimit : pageIndex * cardIncrease;
+
+  cardCountElem.innerHTML = endRange;
+
+  for (let i = startRange + 1; i <= endRange; i++) {
+    createCard(i, cardImages[i - 1]); // Pass the image URL for each card
+  }
+};
+
+window.onload = function () {
+  addCards(currentPage);
+  // loadMoreButton.style.backgroundColor = getRandomColor();
+  loadMoreButton.addEventListener("click", () => {
+    addCards(currentPage + 1);
+  });
+};
+
+
+
+
+
+
+
+
+
+
+
+
